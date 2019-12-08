8d08e531f58f027c8fecff18d69aa7854cdd1fa5
周涛 <zhoutaotao@aliyun.com>
2019-12-08 18:46:38 +0800

    :pencil: 新增动态编译说明文件

--- dynamic_compile/src/com/taoes/compile/JavaCodeEval.java
+++ dynamic_compile/src/com/taoes/compile/JavaCodeEval.java
@@ -17,11 +17,15 @@ public class JavaCodeEval implements Compile {
 
     @Override
     public List<Class> process(List<ClassSource> classSourcesList) throws IOException {
+        // 获取系统编译器
         JavaCompiler javaCompiler = ToolProvider.getSystemJavaCompiler();
+
+        // 获取文件管理器
         StandardJavaFileManager standardFileManager = javaCompiler.getStandardFileManager(null, null, null);
 
         Iterable<? extends SimpleJavaFileObject> javaObjects = classSourcesList.stream().map(ClassSource::toStringJavaObject).collect(Collectors.toList());
 
+        // 构建编译输出目录
         String outDir = null;
         try {
             File file = new File(Objects.requireNonNull(Thread.currentThread().getContextClassLoader().getResource("")).toURI());
@@ -30,11 +34,14 @@ public class JavaCodeEval implements Compile {
             e.printStackTrace();
         }
 
+        // 构建编译选项
         Iterable<String> optional = Arrays.asList("-d", outDir);
         JavaCompiler.CompilationTask task = javaCompiler.getTask(null, standardFileManager, null, optional, null, javaObjects);
 
+        // 执行编译
         if (task.call()) {
             try {
+                // 遍历加载类到方法区
                 List<Class> classList = new ArrayList<>(classSourcesList.size());
                 for (ClassSource classSource : classSourcesList) {
                     String className = classSource.getClassName();
